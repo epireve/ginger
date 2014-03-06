3aab2936b2974136e6b4eff5d34a1e03687a837f
Firdaus Adib <i@firdaus.my>
2014-03-07 03:21:54 +0800

    Push fresh rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Orangeginger::Application.config.secret_key_base = '3a99e2918048a754decf1a6248ab73c2d8358840f10679a10813808ab71707fa30a2ee356301fa26e4e1615317b759a07046fe755b8a8bb6021185cecfc90e10'
