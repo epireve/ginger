d365e88ec15b35fddb7f4c6d57090b7539343a63
Firdaus Adib <i@firdaus.my>
2014-04-20 18:23:22 +0800

    Changed main page to tweets#index

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Orangeginger::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'searches#index'
+   root 'tweets#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
